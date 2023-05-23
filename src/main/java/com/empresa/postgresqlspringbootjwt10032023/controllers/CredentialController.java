0bf6d45204e7b0c9b682e655a0786ad05e4910a8
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-05-23 09:51:41 -0300

    feature-22-credential-post

---
+++ src/main/java/com/empresa/postgresqlspringbootjwt10032023/controllers/CredentialController.java
@@ -0,0 +1,23 @@
+package com.empresa.postgresqlspringbootjwt10032023.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.empresa.postgresqlspringbootjwt10032023.services.CredentialService;
+import com.empresa.postgresqlspringbootjwt10032023.models.Credential;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/credentials")
+public class CredentialController {
+
+    CredentialService credentialService = new CredentialService();
+
+    @PostMapping
+    public void store(@RequestBody Credential credential) {
+        credentialService.store(credential);
+    }
+}
