c27e11914a3c0f07a35501df067d362a07c25ab7
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-06-13 11:29:29 -0300

    feature-29-check-token

--- src/main/java/com/empresa/postgresqlspringbootjwt10032023/controllers/AuthController.java
+++ src/main/java/com/empresa/postgresqlspringbootjwt10032023/controllers/AuthController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import com.empresa.postgresqlspringbootjwt10032023.models.Credential;
+import com.empresa.postgresqlspringbootjwt10032023.models.CheckToken;
 import com.empresa.postgresqlspringbootjwt10032023.services.CredentialService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.http.HttpHeaders;
+import io.jsonwebtoken.Jwts;
 
 @CrossOrigin
 @RestController
@@ -19,6 +24,35 @@ public class AuthController {
 
     public AuthController(CredentialService credentialService) {
         this.credentialService = credentialService;
+    }
+
+    private CheckToken checkTokenResponse(Boolean statusToken, String statusError) {
+        CheckToken checkToken = new CheckToken();
+        checkToken.setStatusToken(statusToken);
+        checkToken.setStatusError(statusError);
+        return checkToken;
+    }
+
+    @GetMapping("/check-token")
+    public ResponseEntity<?> checkToken(@RequestHeader HttpHeaders httpHeaders) {
+        String authorization = httpHeaders.getFirst("authorization");
+
+        if (authorization == null) {
+            return new ResponseEntity<>(checkTokenResponse(false, "Authorization is empty"), HttpStatus.OK);
+        }
+
+        if (!authorization.split(" ")[0].equals("Bearer")) {
+            return new ResponseEntity<>(checkTokenResponse(false, "Authorization type Bearer was not sent"), HttpStatus.OK);
+        }
+
+        String token = authorization.split(" ")[1];
+
+        try {
+            Jwts.parser().setSigningKey("SECRET_KEY").parseClaimsJws(token);
+            return new ResponseEntity<>(checkTokenResponse(true, ""), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(checkTokenResponse(false, e.getMessage()), HttpStatus.OK);
+        }
     }
 
     @PostMapping("/signin")
