d6b1e2282eb321caabec82c9fb7d36cc6bb6f481
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-07-18 09:26:25 -0300

    feature-36-get-user-with-credentials

--- src/main/java/com/empresa/postgresqlspringbootjwt10032023/repositories/UserRepository.java
+++ src/main/java/com/empresa/postgresqlspringbootjwt10032023/repositories/UserRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 import com.empresa.postgresqlspringbootjwt10032023.models.User;
+import com.empresa.postgresqlspringbootjwt10032023.models.UserEntity;
+import com.empresa.postgresqlspringbootjwt10032023.models.Credential;
 
 public class UserRepository {
     public List<User> findAll() {
@@ -39,31 +41,57 @@ public class UserRepository {
         return users;
     }
 
-    public User findUserById(int id) {
+    public UserEntity findUserById(int id) {
 
-        User user = new User();
+        UserEntity userEntity = new UserEntity();
+        List<Credential> credentials = new ArrayList<Credential>();
 
         try {
             String url = "jdbc:postgresql://localhost:5432/postgresql_springboot_jwt_10032023_db";
             String myUser = "postgres";
             String myPassword = "postgres";
-            String sql_query = "select * from users where id = ?;";
+            String sql_query = ""
+                + "select "
+                + "u.id as userId,"
+                + "u.name,"
+                + "u.created_at as userCreatedAt,"
+                + "u.updated_at as userUpdatedAt,"
+                + "c.id as credentialId,"
+                + "c.user_id as credentialUserId,"
+                + "c.email,"
+                + "c.password,"
+                + "c.created_at as credentialCreatedAt,"
+                + "c.updated_at as credentialUpdatedAt "
+                + "from users u "
+                + "left join credentials c on c.user_id = u.id "
+                + "where u.id = ?;";
             Connection connection = DriverManager.getConnection(url, myUser, myPassword);
             PreparedStatement preparedStatement = connection.prepareStatement(sql_query);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                user.setId(resultSet.getInt("id"));
-                user.setName(resultSet.getString("name"));
-                user.setCreatedAt(resultSet.getTimestamp("created_at"));
-                user.setUpdatedAt(resultSet.getTimestamp("updated_at"));
+                Credential credential = new Credential();
+
+                userEntity.setId(resultSet.getInt("userId"));
+                userEntity.setName(resultSet.getString("name"));
+                userEntity.setCreatedAt(resultSet.getTimestamp("userCreatedAt"));
+                userEntity.setUpdatedAt(resultSet.getTimestamp("userUpdatedAt"));
+
+                credential.setId(resultSet.getInt("credentialId"));
+                credential.setUserId(resultSet.getInt("credentialUserId"));
+                credential.setEmail(resultSet.getString("email"));
+                credential.setPassword(resultSet.getString("password"));
+                credential.setCreatedAt(resultSet.getTimestamp("credentialCreatedAt"));
+                credential.setUpdatedAt(resultSet.getTimestamp("credentialUpdatedAt"));
+                credentials.add(credential);
             }
         } catch (SQLException e) {
             e.printStackTrace(System.err);
         }
 
-        return user;
+        userEntity.setCredentials(credentials);
+        return userEntity;
     }
 
     public int save(User user) {
