9574a43a69b3ade9b4b43412cea6cc768e5505aa
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-08-08 11:30:06 -0300

    feature-39-post-clinic

---
+++ src/main/java/com/empresa/postgresqlspringbootjwt10032023/services/ClinicService.java
@@ -0,0 +1,14 @@
+package com.empresa.postgresqlspringbootjwt10032023.services;
+
+import com.empresa.postgresqlspringbootjwt10032023.repositories.ClinicRepository;
+import com.empresa.postgresqlspringbootjwt10032023.models.Clinic;
+import com.empresa.postgresqlspringbootjwt10032023.models.ClinicResponse;
+
+public class ClinicService {
+
+    ClinicRepository clinicRepository = new ClinicRepository();
+
+    public ClinicResponse store(Clinic clinic) {
+        return clinicRepository.save(clinic);
+    }
+}
